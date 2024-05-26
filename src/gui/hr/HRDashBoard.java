6da69f0d5512a5487f59069fe2dc235605e8c301
Thipul Liyanage <thipuldamsilu@gmail.com>
2024-05-27 00:13:24 +0530

    dashboard integrated

--- src/gui/hr/HRDashBoard.java
+++ src/gui/hr/HRDashBoard.java
@@ -1,16 +1,104 @@
 package gui.hr;
 
+import javaswingdev.drawer.Drawer;
 import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import java.awt.BorderLayout;
 import java.sql.ResultSet;
+import javaswingdev.drawer.DrawerController;
+import javaswingdev.drawer.DrawerItem;
+import javaswingdev.drawer.EventDrawer;
+import javax.swing.SwingUtilities;
+import model.DrawerHeader;
 
 public class HRDashBoard extends javax.swing.JFrame {
 
+    private DrawerController drawer;
+
     public HRDashBoard(ResultSet loggedUser) {
         initComponents();
+        drawer = Drawer.newDrawer(this)
+                .header(new DrawerHeader())
+                .enableScroll(true)
+                .addChild(new DrawerItem("Dashboard").build())
+                .addChild(new DrawerItem("Available Spaces").build())
+                .addChild(new DrawerItem("Parking Manage").build())
+                .addChild(new DrawerItem("Ticket Manage").build())
+                .addFooter(new DrawerItem("Log out").build())
+                .event(new EventDrawer() {
+                    @Override
+                    public void selected(int i, DrawerItem di) {
+                        if (i != 4) { // logout index
+                            setActiveTab(i);
+                        } else {
+                            System.out.println("Log out");
+                        }
+                    }
+                }).build();
+        
+        Dashboard dashboard = new Dashboard();
+        bottomPanel.add(dashboard, BorderLayout.CENTER);
+        SwingUtilities.updateComponentTreeUI(this);
+        repaint();
     }
-    
+
     public HRDashBoard() {
         initComponents();
+        drawer = Drawer.newDrawer(this)
+                .header(new DrawerHeader())
+                .enableScroll(true)
+                .addChild(new DrawerItem("Dashboard").build())
+                .addChild(new DrawerItem("Available Spaces").build())
+                .addChild(new DrawerItem("Parking Manage").build())
+                .addChild(new DrawerItem("Ticket Manage").build())
+                .addFooter(new DrawerItem("Log out").build())
+                .event(new EventDrawer() {
+                    @Override
+                    public void selected(int i, DrawerItem di) {
+                        if (i != 4) { // logout index
+                            setActiveTab(i);
+                        } else {
+                            System.out.println("Log out");
+                        }
+                    }
+                }).build();
+        
+        Dashboard dashboard = new Dashboard();
+        bottomPanel.add(dashboard, BorderLayout.CENTER);
+        SwingUtilities.updateComponentTreeUI(this);
+        repaint();
+    }
+    
+    private void setActiveTab(int tab) { // tab -> clicked tab index
+        bottomPanel.removeAll();
+
+        if (tab == 0) {
+            Dashboard dashboard = new Dashboard();
+            bottomPanel.add(dashboard, BorderLayout.CENTER);
+        } else if (tab == 1) {
+//            Available available = new Available(drawer);
+//            bottomPanel.add(available, BorderLayout.CENTER);
+        } else if (tab == 2) {
+//            Parking parking = new Parking(drawer);
+//            bottomPanel.add(parking, BorderLayout.CENTER);
+        } else if (tab == 3) {
+//            Ticket ticket = new Ticket(drawer);
+//            bottomPanel.add(ticket, BorderLayout.CENTER);
+        }
+
+        drawer.hide();
+
+        SwingUtilities.updateComponentTreeUI(this);
+        repaint();
+    }
+    
+    
+
+    private void toggleMenu() {
+        if (drawer.isShow()) {
+            drawer.hide();
+        } else {
+            drawer.show();
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -18,40 +106,60 @@ public class HRDashBoard extends javax.swing.JFrame {
     private void initComponents() {
 
         background = new javax.swing.JPanel();
+        topPanel = new javax.swing.JPanel();
+        menuButton = new javax.swing.JButton();
+        bottomPanel = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(900, 600));
         setResizable(false);
+        getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.LINE_AXIS));
 
-        javax.swing.GroupLayout backgroundLayout = new javax.swing.GroupLayout(background);
-        background.setLayout(backgroundLayout);
-        backgroundLayout.setHorizontalGroup(
-            backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+        background.setLayout(new java.awt.BorderLayout());
+
+        topPanel.setPreferredSize(new java.awt.Dimension(666, 30));
+
+        menuButton.setText("|||");
+        menuButton.setBorder(null);
+        menuButton.setMargin(new java.awt.Insets(0, 0, 0, 0));
+        menuButton.setPreferredSize(new java.awt.Dimension(50, 50));
+        menuButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout topPanelLayout = new javax.swing.GroupLayout(topPanel);
+        topPanel.setLayout(topPanelLayout);
+        topPanelLayout.setHorizontalGroup(
+            topPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(topPanelLayout.createSequentialGroup()
+                .addComponent(menuButton, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 540, Short.MAX_VALUE))
         );
-        backgroundLayout.setVerticalGroup(
-            backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+        topPanelLayout.setVerticalGroup(
+            topPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(menuButton, javax.swing.GroupLayout.DEFAULT_SIZE, 30, Short.MAX_VALUE)
         );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        background.add(topPanel, java.awt.BorderLayout.PAGE_START);
+
+        bottomPanel.setLayout(new javax.swing.BoxLayout(bottomPanel, javax.swing.BoxLayout.LINE_AXIS));
+        background.add(bottomPanel, java.awt.BorderLayout.CENTER);
+
+        getContentPane().add(background);
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+    private void menuButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuButtonActionPerformed
+        toggleMenu();
+    }//GEN-LAST:event_menuButtonActionPerformed
+
     public static void main(String args[]) {
         FlatMacLightLaf.setup();
-        
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new HRDashBoard().setVisible(true);
@@ -61,5 +169,8 @@ public class HRDashBoard extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel background;
+    private javax.swing.JPanel bottomPanel;
+    private javax.swing.JButton menuButton;
+    private javax.swing.JPanel topPanel;
     // End of variables declaration//GEN-END:variables
 }
